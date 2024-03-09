4a9426aa339d4541a392272aebaf7c522a6fb2b8
Alan Jones Rios <alan.jones.rios@gmail.com>
2024-03-08 21:29:52 -0300

    chore: updated social card image format - The social card image format has been changed from png to jpg.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/mfdocs.png',
+    image: 'img/mfdocs.jpg',
     navbar: {
       title: '',
       logo: {
